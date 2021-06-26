19a7cbc90aa0b4ad510ff1506b607808c9962a47
Sladjana1992 <slax01@live.de>
2021-06-26 09:08:06 +0200

    some done

--- exem-stat/js/app.js
+++ exem-stat/js/app.js
@@ -7,28 +7,48 @@ var addNewSubjectNode = document.querySelector("#adding-subject");
 var addStudentToPassListNode = document.querySelector("#passed-list");
 var addStudentToFailListNode = document.querySelector("#failed-list");
 var buttonNode = document.querySelector("#btn");
+var headStatOfFailNode = document.querySelector("#add-stat-fail");
+var headStatOfPassNode = document.querySelector(".add-stat-pass");
 
+
+var addPassed = [];
+var addFailed = [];
 
 function addStudentToListHandler () {
     //create student, grade and subject (exam)
-    var student = new Student (studentFullNameValidNode.value)
     var addingStudentExam = new Exam (addNewSubjectNode.value, studentFullNameValidNode.value,
         studentGradeValidNode.value);
     var checkIfStudentPassOrFail = addingStudentExam.hasPassed();
     //create li element
     var createLiElementNode = document.createElement("li");
     //check if student pass or fail the exam and put him/her in the corect list
+    
+
     if(checkIfStudentPassOrFail === "Passed"){
+        addPassed.push(addingStudentExam);
         studentList.addStudentToPassList(addingStudentExam);
-        createLiElementNode.textContent = addingStudentExam.getExamInfo();
+        createLiElementNode.textContent = addingStudentExam.getExamInfo() + "    " + 
+        studentGradeValidNode.value;
         addStudentToPassListNode.appendChild(createLiElementNode);
+
+        headStatOfPassNode.innerHTML = addPassed.length;
         
     }else if(checkIfStudentPassOrFail === "Failed") {
+        addFailed.push(addingStudentExam);
         studentList.addStudentToFailList(addingStudentExam);
-        createLiElementNode.textContent = addingStudentExam.getExamInfo();
+        createLiElementNode.textContent = addingStudentExam.getExamInfo() + "    " +
+        studentGradeValidNode.value;
         addStudentToFailListNode.appendChild(createLiElementNode);
-    }
+        
+        var procentOfFail = Math.floor(addFailed.length/(addPassed.length + addFailed.length)*100) + "%";
+        headStatOfFailNode.textContent = addFailed.length + " " + procentOfFail;
 
+
+
+    }
+    addNewSubjectNode.value = "JavaScript";
+    studentFullNameValidNode.value = "";
+    studentGradeValidNode.value = "";
 }
 
 
